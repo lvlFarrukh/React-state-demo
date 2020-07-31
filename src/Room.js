4385419df9378ab4fdfb0968cc881906d3682154
Farrukh Aslam <farrukha303@gmail.com>
2020-07-31 09:25:44 -0700

    updated

--- src/Room.js
+++ src/Room.js
@@ -1,13 +1,14 @@
 import React, {useState} from 'react';
 import './App.css';
+import './room.css';
 
 function Room() {
     let [isLit, setLit] = useState(true);
     let [age, setAge] = useState(18);
     // console.log(setLit)
 
-  return (
-    <div>
+  return  (
+    <div className={'room '+(isLit? 'lit': 'dark')}>
         <h1>This room is {isLit ? 'Lite': 'Dark'}</h1>
         <h1>Age: {age}</h1>
         <button onClick={() => {
